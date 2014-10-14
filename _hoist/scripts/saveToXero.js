c15a0e193d164c268bd593ad48ef9e282936941f
Owen Evans <owen@bgeek.net>
2014-10-14 16:47:04 +1300

    fix comment

--- _hoist/scripts/saveToXero.js
+++ _hoist/scripts/saveToXero.js
@@ -6,8 +6,9 @@ module.exports = function (event) {
   //put the new invoice
   xero.put('/invoice', invoice, function (err) {
     if (err) {
-      //these two lines do the same thing
+      //these two lines do the same thing in that they both log an error message
       hoist.error(err);
+      //this one will also mark the module run as failed with an error
       throw err;
     } else {
       hoist.raise('save.invoice.done', invoice);
