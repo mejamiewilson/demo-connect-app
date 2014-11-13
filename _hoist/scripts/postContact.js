c614c1e179c04b37cddb97e9771c3318ff8b0a0b
Owen Evans <owen@bgeek.net>
2014-11-14 11:13:16 +1300

    just log event to start

--- _hoist/scripts/postContact.js
+++ _hoist/scripts/postContact.js
@@ -1,5 +1,8 @@
 'use strict';
-var request = require('request');
+//var request = require('request');
+
 module.exports = function(ev,done){
-    request.post('http://proxyhoiio.ngrok.com',ev,done);
+  console.log('inside postContact',ev);
+  done();
+  //request.post('http://proxyhoiio.ngrok.com',ev,done);
 };
