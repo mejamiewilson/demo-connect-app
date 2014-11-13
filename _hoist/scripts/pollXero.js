57384208f5f86971978fcf9334e3da04acbe75a3
Owen Evans <owen@bgeek.net>
2014-11-13 22:21:44 +1300

    add xero call

--- _hoist/scripts/pollXero.js
+++ _hoist/scripts/pollXero.js
@@ -2,16 +2,21 @@
 var request = require('request');
 module.exports = function (ev, done) {
   console.log('in poll xero');
-  request.post('http://proxyhoiio.ngrok.com', {
-      form: {
-        event: ev
-      }
-    })
-    .on('error', function (err) {
-      console.log('error during request', err);
-      done();
-    }).on('response', function (response) {
-      console.log('got response', response);
-      done();
-    });
+  Hoist.connector('hoist-connector-xero', 'xero').get('/contacts').then(function (result) {
+    console.log('result:', result);
+    request.post('http://proxyhoiio.ngrok.com', {
+        form: {
+          xero: result
+        }
+      })
+      .on('error', function (err) {
+        console.log('error during request', err);
+        done();
+      }).on('response', function (response) {
+        console.log('got response', response);
+        done();
+      });
+  }).catch(function (err) {
+    console.log('error with xero connector', err);
+  });
 };
