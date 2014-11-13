60ff91fe477bd35561a31013097fd113bf8e3d7b
Owen Evans <owen@bgeek.net>
2014-11-14 11:34:40 +1300

    use core request method

--- _hoist/scripts/postContact.js
+++ _hoist/scripts/postContact.js
@@ -1,7 +1,15 @@
 'use strict';
 var request = require('request');
 
-module.exports = function(ev,done){
-  console.log('inside postContact',ev);
-  request.post('http://proxyhoiio.ngrok.com',{json:ev.payload},done);
+module.exports = function (ev, done) {
+  console.log('inside postContact', ev);
+  request({
+    url: 'http://proxyhoiio.ngrok.com',
+    method: 'POST',
+    json: ev.payload
+  }, function (err, response) {
+    console.log('response status', response.statusCode);
+    console.log('error', err);
+    done();
+  });
 };
