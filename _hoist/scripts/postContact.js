488afd330301aefabdfc931cc51364f3677e3ee3
Owen Evans <owen@bgeek.net>
2014-11-14 11:21:01 +1300

    fix posting contact

--- _hoist/scripts/postContact.js
+++ _hoist/scripts/postContact.js
@@ -1,8 +1,7 @@
 'use strict';
-//var request = require('request');
+var request = require('request');
 
 module.exports = function(ev,done){
   console.log('inside postContact',ev);
-  done();
-  //request.post('http://proxyhoiio.ngrok.com',ev,done);
+  request.post('http://proxyhoiio.ngrok.com',{json:ev.payload},done);
 };
