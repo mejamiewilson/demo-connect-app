234814a62e2a1e4d55bad1b447c87291c0398f7a
Owen Evans <owen@bgeek.net>
2014-11-13 21:59:24 +1300

    make request a post

--- _hoist/scripts/pollXero.js
+++ _hoist/scripts/pollXero.js
@@ -1,13 +1,17 @@
 'use strict';
 var request = require('request');
-module.exports = function(ev,done){
+module.exports = function (ev, done) {
   console.log('in poll xero');
-  request.get('http://proxyhoiio.ngrok.com')
-  .on('error', function(err) {
-    console.log('error during request',err);
-    done();
-  }).on('response',function(response){
-    console.log('got response',response);
-    done();
-  });
+  request.post('http://proxyhoiio.ngrok.com', {
+      form: {
+        event: ev
+      }
+    })
+    .on('error', function (err) {
+      console.log('error during request', err);
+      done();
+    }).on('response', function (response) {
+      console.log('got response', response);
+      done();
+    });
 };
