33fd289b97ad8bb838724f3495dbfb8717bbb198
Owen Evans <owen@bgeek.net>
2014-11-27 00:07:14 +1300

    make sure log finishes before quit

--- _hoist/scripts/pingLog.js
+++ _hoist/scripts/pingLog.js
@@ -1,5 +1,6 @@
 'use strict';
-module.exports = function(ev,done){
-  Hoist.log('got a ping',ev);
-  done();
+module.exports = function (ev, done) {
+  Hoist.log('got a ping', ev).then(function () {
+    done();
+  });
 };
