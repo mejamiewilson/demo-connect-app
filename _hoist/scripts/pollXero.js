82726ae3de3205605a59860c6e86856799b05952
Owen Evans <owen@bgeek.net>
2014-11-27 23:34:22 +1300

    reset to true events

--- _hoist/scripts/pollXero.js
+++ _hoist/scripts/pollXero.js
@@ -1,30 +1,27 @@
 'use strict';
 
-//var BBPromise = require('bluebird');
-//var _ = require('lodash');
+var BBPromise = require('bluebird');
+var _ = require('lodash');
 
 
 module.exports = function (ev, done) {
-  Hoist.log('in poll xero').then(function(){
+  Hoist.log('in poll xero').then(function () {
 
-  return Hoist.connector('hoist-connector-xero', 'xero')
-    .get('/contacts')
-    .then(function () {
-      /*
-      if (result.Response.Contacts.Contact) {
-        var eventsRaised = _.map(result.Response.Contacts.Contact, function (contact) {
-          return Hoist.events.raise('xero:contact', {
-            contact: contact
+    return Hoist.connector('hoist-connector-xero', 'xero')
+      .get('/contacts')
+      .then(function (result) {
+
+        if (result.Response.Contacts.Contact) {
+          var eventsRaised = _.map(result.Response.Contacts.Contact, function (contact) {
+            return Hoist.events.raise('xero:contact', {
+              contact: contact
+            });
           });
-        });
-        return BBPromise.all(eventsRaised);
+          return BBPromise.all(eventsRaised);
 
-      }
-      */
-      throw new Error('i created an error');
-      return Hoist.log('got response from xero');
+        }
+        return Hoist.log('got response from xero');
 
-    });
+      });
   }).nodeify(done);
 };
-
--- _hoist/scripts/postContact.js
+++ _hoist/scripts/postContact.js
@@ -1,17 +1,7 @@
 'use strict';
 
-var request = require('request');
 
 module.exports = function (ev, done) {
-  Hoist.log('inside postContact', ev);
-  request({
-    url: 'http://proxyhoiio.ngrok.com',
-    method: 'POST',
-    json: ev.payload
-  }, function (err, response) {
-    Hoist.log('response status', response.statusCode);
-    Hoist.log('error', err);
-    done();
-  });
+  Hoist.log('got a contact', JSON.stringify(ev.payload));
+  done();
 };
-
