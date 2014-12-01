fded34896104fbf7a7b0ee73c6791ac51f266c6d
Owen Evans <owen@bgeek.net>
2014-12-01 20:47:46 +1300

    fix up raising event logging

--- _hoist/scripts/pollXero.js
+++ _hoist/scripts/pollXero.js
@@ -14,9 +14,12 @@ module.exports = function (ev, done) {
         if (result.Response.Contacts.Contact) {
           var index = 0;
           var eventsRaised = _.map(result.Response.Contacts.Contact, function (contact) {
+            console.log('raising event',index);
             return Hoist.events.raise('xero:contact', {
               contact: contact,
               number: index++
+            }).catch(function (err) {
+              return Hoist.log('unable to raise event', index, err.message);
             });
           });
           return BBPromise.all(eventsRaised).then(function () {
