513e26721631ba61643c42eb3a2859d6f833dc40
Owen Evans <owen@bgeek.net>
2014-12-01 21:33:20 +1300

    add data save

--- _hoist/scripts/postContact.js
+++ _hoist/scripts/postContact.js
@@ -3,6 +3,6 @@
 
 module.exports = function (ev, done) {
   Hoist.log('got a contact', ev.payload.number, JSON.stringify(ev.payload.contact.ContactID)).then(function () {
-    done();
-  });
+    return Hoist.data('Contact').save(ev.payload);
+  }).nodeify(done);
 };
