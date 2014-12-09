6042a0c2a1967215745af8b1bc27f6d22cca175f
Owen Evans <owen@bgeek.net>
2014-12-09 21:49:44 +1300

    add public xero connection

--- _hoist/scripts/pollXero.js
+++ _hoist/scripts/pollXero.js
@@ -5,30 +5,11 @@
 
 
 module.exports = function (ev, done) {
-  Hoist.log('in poll xero', ev).then(function () {
-    
-    /*return Hoist.connector('hoist-connector-xero', 'xero')
-      .get('/contacts')
-      .then(function (result) {
-
-        if (result.Response.Contacts.Contact) {
-          var index = 0;
-          var eventsRaised = _.map(result.Response.Contacts.Contact, function (contact) {
-            console.log('raising event',index);
-            return Hoist.events.raise('xero:contact', {
-              contact: contact,
-              number: index++
-            }).catch(function (err) {
-              return Hoist.log('unable to raise event', index, err.message);
-            });
-          });
-          return BBPromise.all(eventsRaised).then(function () {
-            return Hoist.log('recieved', result.Response.Contacts.Contact.length, 'contacts');
-          });
-
-        }
-        return Hoist.log('got response from xero');
-
-      });*/
-  }).nodeify(done);
+  //Hoist.log('in poll xero', ev).then(function () {
+  return Hoist.connector('xero-public')
+    .authorize(ev._request.query.token)
+    .get('/contacts')
+    .then(function (result) {
+      return Hoist.log('got response from xero', result);
+    }).nodeify(done);
 };
